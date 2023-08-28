d285460b9c1600d89d1a63c8c21ea65d8b73cb0f
John Beveridge <john.beveridge@fcc-fac.ca>
2023-08-27 21:02:22 -0600

    misc. changes

--- demo-service/src/main/java/com/example/demo/core/CartItemService.java
+++ demo-service/src/main/java/com/example/demo/core/CartItemService.java
@@ -4,6 +4,8 @@ import com.example.demo.api.CartItemDto;
 import com.example.demo.assembler.CartItemAssembler;
 import com.example.demo.core.data.CartItem;
 import com.example.demo.core.data.CartItemRepository;
+import com.example.demo.core.data.CartRepository;
+import com.example.demo.core.data.ItemRepository;
 import com.example.demo.core.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,8 @@ public class CartItemService {
 
     private final CartItemRepository repository;
     private final CartItemAssembler assembler;
+    private final CartRepository cartRepository;
+    private final ItemRepository itemRepository;
 
     public List<CartItemDto> findAll(){
         List<CartItem> items = repository.findAll();
@@ -33,8 +37,12 @@ public class CartItemService {
                 .orElseThrow(NotFoundException::new);
     }
 
-//    TODO should cart item be an Optional because Item will be null on creation
+//    TODO what is the functional way to check for existence of entities before saving?
     public CartItemDto create(CartItemDto aCartItem){
+        if(! itemRepository.existsById(aCartItem.getItemId()))
+            throw new NotFoundException();
+        if(! cartRepository.existsById(aCartItem.getCartId()))
+            throw new NotFoundException();
         CartItem cartItem = assembler.disassemble(aCartItem);
         cartItem = repository.save(cartItem);
         return assembler.assemble(cartItem);
